a0a4c19f182e49edf36ed2da4b05a9e744262a38
DanilodeRamos <Danilodramos@proton.me>
2024-11-13 10:33:06 -0300

    SQL, projection, get games by list

--- dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,14 +1,16 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
+
 	private Long id;
 	private String title;
 	private Integer year;
 	private String imgUrl;
 	private String shortDescription;
-	
+
 	public GameMinDTO(Game entity) {
 		id = entity.getId();
 		title = entity.getTitle();
@@ -16,18 +18,30 @@ public class GameMinDTO {
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
+
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
 	public Long getId() {
 		return id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public Integer getYear() {
 		return year;
 	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public String getShortDescription() {
 		return shortDescription;
 	}
